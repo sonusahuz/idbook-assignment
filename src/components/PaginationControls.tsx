da87e32bf3b7a909d03733ba4883b54432ea30f5
sonusahuz <sonusahu8930@gmail.com>
2024-12-28 11:25:55 +0530

    fix:some-styling

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -23,16 +23,28 @@ type Props = {
 const PaginationControls = ({ table, users }: Props) => {
   return (
     <div>
-      <div className="mt-4 flex items-center justify-between">
-        <div>
+      <div className="mt-4 flex items-center justify-between flex-wrap">
+        <div className="text-center mx-auto md:mx-0 mb-4 md:mb-0">
           Showing {table.getState().pagination.pageSize} of {users.length} users
         </div>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-2 mx-auto md:mx-0">
           <button
             onClick={() => table.previousPage()}
             disabled={!table.getCanPreviousPage()}
-            className="px-2 py-1 border-2 rounded disabled:opacity-50 w-24"
+            className="px-2 py-1 border-2 rounded disabled:opacity-50 flex items-center justify-between gap-4 font-normal"
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="size-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M17 10a.75.75 0 0 1-.75.75H5.612l4.158 3.96a.75.75 0 1 1-1.04 1.08l-5.5-5.25a.75.75 0 0 1 0-1.08l5.5-5.25a.75.75 0 1 1 1.04 1.08L5.612 9.25H16.25A.75.75 0 0 1 17 10Z"
+                clipRule="evenodd"
+              />
+            </svg>
             Previous
           </button>
           <span>
@@ -42,9 +54,21 @@ const PaginationControls = ({ table, users }: Props) => {
           <button
             onClick={() => table.nextPage()}
             disabled={!table.getCanNextPage()}
-            className="px-2 py-1 border-2 rounded disabled:opacity-50 w-24"
+            className="px-2 py-1 border-2 rounded disabled:opacity-50 flex items-center justify-between gap-4 font-normal"
           >
             Next
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="size-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 10a.75.75 0 0 1 .75-.75h10.638L10.23 5.29a.75.75 0 1 1 1.04-1.08l5.5 5.25a.75.75 0 0 1 0 1.08l-5.5 5.25a.75.75 0 1 1-1.04-1.08l4.158-3.96H3.75A.75.75 0 0 1 3 10Z"
+                clipRule="evenodd"
+              />
+            </svg>
           </button>
         </div>
       </div>
